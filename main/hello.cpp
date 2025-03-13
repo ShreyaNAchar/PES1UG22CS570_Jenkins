e695cf412440c454920bb2bd1712e9181b556350
SHREYA ACHAR <163900522+ShreyaNAchar@users.noreply.github.com>
2025-03-13 16:23:18 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "NAME: SHREYA ACHAR \nSRN: PES1UG22CS570\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
